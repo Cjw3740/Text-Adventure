91d7ba6a46510aa1d7e914642b5ab5a884b5bb16
Cjw3740 <35120764+Cjw3740@users.noreply.github.com>
2018-01-27 14:09:16 -0500

    Trying out Gihub from within Atom

--- Xortt.py
+++ Xortt.py
@@ -6,11 +6,20 @@ __  __          _   _   \n\
 /_/\_\___/|_|   \__|\__|"
 
 
+look_syn = ["look","examine","study"]
+move_syn = ["go","walk","run","skip","travel","saunter","crawl","move"]
+directions = ["north","south","east","west","up","down","in","out"]
 
 
 
 
 
+
+def input_handler(cmd):
+    cmdList = cmd.split(" ")
+    if look_syn in cmdList:
+        if "around" in cmdList:
+            print("this is a test of looking around")
 
 
 
@@ -20,3 +29,7 @@ __  __          _   _   \n\
 save_exit = False
 while not save_exit:
   cmd = input("What would you like to do?   ").lower()
+  if cmd == "exit":
+      save_exit = True #will need to add save to this
+  else:
+      input_handler(cmd)
